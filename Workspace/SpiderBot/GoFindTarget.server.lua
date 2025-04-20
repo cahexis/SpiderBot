7a9e4c9937cf8393cd4de9e40876d4dbd345a449
Emi <183315760+cahexis@users.noreply.github.com>
2025-04-19 21:00:14 -0500

    Create GoFindTarget.server.lua

---
+++ Workspace/SpiderBot/GoFindTarget.server.lua
@@ -0,0 +1,47 @@
+local bot = script.Parent
+local root = bot.HumanoidRootPart
+local bothum = bot.Humanoid
+local bind = bot.Target
+local pfs = game:GetService("PathfindingService")
+local path = pfs:CreatePath()
+local rs = game:GetService("RunService")
+local runningtime 
+local anim = Instance.new("Animation") --walk
+anim.AnimationId = "rbxassetid://124480240466645"
+local track = bothum:LoadAnimation(anim)
+local bombsound = bot.bomb.bombtick
+local firesound = bot.bomb.firesound
+local fireemit = bot.FireEmitter.Fire
+local boom = Instance.new("Explosion")
+boom.BlastRadius = 10
+boom.BlastPressure = 10
+
+track.Looped = true
+
+local function bombtick()
+	local tickcount = 1
+	while tickcount > 0.1 do
+		wait(tickcount)
+		bombsound:Play()
+		tickcount = tickcount - 0.1
+	end
+end
+
+bind.Event:Connect(function(closest)
+	wait(0.2)
+	track:Play()
+	runningtime = rs.Stepped:Connect(function()
+		bothum:MoveTo(closest.HumanoidRootPart.Position)
+	end)
+end)
+
+bind.Event:Connect(function(closest)
+	firesound:Play()
+	fireemit.Enabled = true
+	bombtick()
+	bombsound:Stop()
+	firesound:Stop()
+	boom.Parent = bot.bomb
+	boom.Position = bot.bomb.Position	
+	bot.HumanoidRootPart:Destroy()
+end)
